a84415f66520f027bcee4e619a19b5a751f1409d
Camilo Jimenez <johan.jimenez@mercadolibre.com.co>
2023-12-01 18:17:13 -0500

    :hammer: feat: new text and footer

--- src/components/about-us/about-us.jsx
+++ src/components/about-us/about-us.jsx
@@ -11,7 +11,8 @@ const AboutUs = () => (
     <div className="gap-16 items-center pt-40 pb-20 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6 text-center">
       <div className="font-light sm:text-lg">
         <h2 className="mb-8 text-4xl tracking-tight font-extrabold">Conoce Nuestra Pasión por el Freestyle</h2>
-        <p className="mb-4">En FMC, respiramos y vivimos freestyle. Nuestra misión es brindar un espacio donde los apasionados por el arte encuentren la libertad para explorar, aprender y perfeccionar sus habilidades. Desde principiantes hasta expertos, todos son bienvenidos a unirse a nuestra comunidad vibrante y descubrir el increíble mundo del freestyle. Conoce nuestra historia, nuestros valores y cómo estamos transformando la experiencia de practicar freestyle para cada persona que se une a nosotros.</p>
+        <p className="mb-4">¡FMC es el hogar del freestyle! Descubre un espacio seguro donde encuentras la libertad para explorar, aprender y perfeccionar tus habilidades de rimar. Rhythm And Poetry (RAP).</p>
+        <p className="mb-4">¡Prepárate para desatar tu flow! Encuentra tu voz única y domina el arte del freestyle.</p>
       </div>
       <div className="grid grid-cols-2 gap-4 mt-8">
         <img className="w-full rounded-lg" src={Image1} alt="about us image 1" />
--- src/components/banner/banner.jsx
+++ src/components/banner/banner.jsx
@@ -17,7 +17,7 @@ const Banner = () => {
         <div className="grid max-w-screen-xl px-4 py-14 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
           <div className="mr-auto place-self-center lg:col-span-7 banner-ls">
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-3xl xl:text-5xl text-white">Tú escenario de practica de Freestyle</h1>
-            <p className="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 md:text-lg lg:text-x">Libera tú ingenio en nuestra plataforma de freestyle. Descubre tu ritmo único y perfecciona tu arte en cada improvisación junto a nosotros.</p>
+            <p className="max-w-2xl mb-6 font-light text-gray-100 lg:mb-8 md:text-lg lg:text-x">Libera tú ingenio en nuestra plataforma que fusiona el freestyle con la potencia de la inteligencia artificial. Descubre tu ritmo único y perfecciona tu arte en cada improvisación junto a nosotros.</p>
             <ButtonFmc loading={false} text="Comenzar" handleClick={handleClick} />
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
